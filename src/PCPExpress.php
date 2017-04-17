ce1fe816df2e59e3d1f101ece7ed2985054ba12f
bsetiawan88 <bsetiawan88@gmail.com>
2017-04-17 15:58:34 +0700

    add basic function

---
+++ src/PCPExpress.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bagus\PCPExpress;
+
+use Requests;
+
+class PCPExpress
+{
+
+	CONST URL = 'http://27.123.221.139';
+	
+	private $username = 'pcpapi_online';
+	private $password = 'Pcpapi_online123';
+
+	public function setCredentials($username, $password)
+	{
+		$this->username = $username;
+		$this->password = $password;
+		return $this;
+	}
+	
+	public function getOriginAndDestination()
+	{
+		$response = Requests::get(self::URL . '/pcp.api/regiononline.php', [], $this->_getOptions());
+		return $response->body;
+	}
+
+	public function deliveryCharge($from, $thru, $weight) {
+		return Requests::post(self::URL . '/pcp.api/serviceonline.php', [], ['from' => $from, 'thru' => $thru, 'weight' => $weight], $this->_getOptions());
+	}
+	
+	private function _getOptions() {
+		return [
+			'auth' => [$this->username, $this->password]
+		];
+	}
+
+}
