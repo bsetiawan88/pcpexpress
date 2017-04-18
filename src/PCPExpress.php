8cd394793e111bcf880e8ed57b5b2aadc42590b2
bsetiawan88 <bsetiawan88@gmail.com>
2017-04-18 10:57:01 +0700

    update

--- src/PCPExpress.php
+++ src/PCPExpress.php
@@ -28,13 +28,61 @@ class PCPExpress
 				$this->originAndDestination = json_decode($response->body);
 			}
 		}
-		
+
 		return $this->originAndDestination;
 	}
 
 	public function deliveryCharge($from, $thru, $weight)
 	{
 		return Requests::post(self::URL . '/pcp.api/serviceonline.php', [], ['from' => $from, 'thru' => $thru, 'weight' => $weight], $this->_getOptions());
+	}
+
+	public function postShipment($params)
+	{
+		return Requests::post(self::URL . '/pcp.api/ecommerce/service/training/posonline.php', [], $params, $this->_getOptions());
+	}
+	
+	public function checkShipment($awb) {
+		$response = Requests::get(self::URL . '/pcp.api/en/trackingonline.php?noawb=' . $awb, [], $this->_getOptions());
+		return $response->body;
+	}
+	
+	public function getShipmentDataTemplate() {
+		return [
+			'allocation_code' => '',
+			'shipment_date_string' => '',
+			'shipment_time_string' => '',
+			'unique_reference_no' => '',
+			'company_id' => '',
+			'company_name' => '',
+			'city_origin_code' => '',
+			'city_origin_name' => '',
+			'city_destination_code' => '',
+			'city_destination_name' => '',
+			'service_code' => '',
+			'service_name' => '',
+			'quantity' => '',
+			'weight' => '',
+			'insurance_flag' => '',
+			'goods_value_are' => '',
+			'goods_description' => '',
+			'shipper_name' => '',
+			'shipper_province_name' => '',
+			'shipper_district_name' => '',
+			'shipper_subdistrict_name' => '',
+			'shipper_postal_code' => '',
+			'shipper_address' => '',
+			'shipper_handphone_number' => '',
+			'shipper_contact' => '',
+			'recipient_name' => '',
+			'recipient_province_name' => '',
+			'recipient_district_name' => '',
+			'recipient_subdistrict_name' => '',
+			'recipient_postal_code' => '',
+			'recipient_address' => '',
+			'recipient_handphone_number' => '',
+			'recipient_contact' => ''
+		];
 	}
 
 	private function _getOptions()
