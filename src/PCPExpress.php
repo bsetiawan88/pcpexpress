692fedad1ae2e1bd7f3817732a05f260ffc7d9a1
bsetiawan88 <bsetiawan88@gmail.com>
2017-04-18 09:55:14 +0700

    update

--- src/PCPExpress.php
+++ src/PCPExpress.php
@@ -8,9 +8,10 @@ class PCPExpress
 {
 
 	CONST URL = 'http://27.123.221.139';
-	
+
 	private $username = 'pcpapi_online';
 	private $password = 'Pcpapi_online123';
+	private $originAndDestination;
 
 	public function setCredentials($username, $password)
 	{
@@ -18,20 +19,26 @@ class PCPExpress
 		$this->password = $password;
 		return $this;
 	}
-	
+
 	public function getOriginAndDestination()
 	{
-		$response = Requests::get(self::URL . '/pcp.api/regiononline.php', [], $this->_getOptions());
-		if (isset($response->body)) {
-			return json_decode($response->body);
+		if (empty($this->originAndDestination)) {
+			$response = Requests::get(self::URL . '/pcp.api/regiononline.php', [], $this->_getOptions());
+			if (isset($response->body)) {
+				$this->originAndDestination = json_decode($response->body);
+			}
 		}
+		
+		return $this->originAndDestination;
 	}
 
-	public function deliveryCharge($from, $thru, $weight) {
+	public function deliveryCharge($from, $thru, $weight)
+	{
 		return Requests::post(self::URL . '/pcp.api/serviceonline.php', [], ['from' => $from, 'thru' => $thru, 'weight' => $weight], $this->_getOptions());
 	}
-	
-	private function _getOptions() {
+
+	private function _getOptions()
+	{
 		return [
 			'auth' => [$this->username, $this->password]
 		];
