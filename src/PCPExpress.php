82f5ce2421be7f022d054882eb8c8380b982e793
bsetiawan88 <bsetiawan88@gmail.com>
2017-05-16 07:01:10 +0700

    update

--- src/PCPExpress.php
+++ src/PCPExpress.php
@@ -7,23 +7,29 @@ use Requests;
 class PCPExpress
 {
 
-	CONST URL = 'http://27.123.221.139';
-
-	private $username = 'pcpapi_online';
-	private $password = 'Pcpapi_online123';
+	private $username;
+	private $password;
+	private $url;
 	private $originAndDestination;
 
-	public function setCredentials($username, $password)
+	public function setCredentials($username, $password, $url)
 	{
 		$this->username = $username;
 		$this->password = $password;
+		$this->url = $url;
 		return $this;
+	}
+	
+	public function setEnvironment($environment) {
+		if ($environment == 'development' || $environment == 'production') {
+			$this->environment = $environment;
+		}
 	}
 
 	public function getOriginAndDestination()
 	{
 		if (empty($this->originAndDestination)) {
-			$response = Requests::get(self::URL . '/pcp.api/regiononline.php', [], $this->_getOptions());
+			$response = Requests::get($this->url . '/pcp.api/regiononline.php', [], $this->_getOptions());
 			if (isset($response->body)) {
 				$this->originAndDestination = json_decode($response->body);
 			}
@@ -34,20 +40,24 @@ class PCPExpress
 
 	public function deliveryCharge($from, $thru, $weight)
 	{
-		return Requests::post(self::URL . '/pcp.api/serviceonline.php', [], ['from' => $from, 'thru' => $thru, 'weight' => $weight], $this->_getOptions());
+		$response = Requests::post($this->url . '/pcp.api/serviceonline.php', [], ['from' => $from, 'thru' => $thru, 'weight' => $weight], $this->_getOptions());
+		return json_decode($response->body);
 	}
 
 	public function postShipment($params)
 	{
-		return Requests::post(self::URL . '/pcp.api/ecommerce/service/training/posonline.php', [], $params, $this->_getOptions());
+		$response =  Requests::post($this->url . '/pcp.api/ecommerce/service/training/posonline.php', [], $params, $this->_getOptions());
+		return json_decode($response->body);
 	}
-	
-	public function checkShipment($awb) {
-		$response = Requests::get(self::URL . '/pcp.api/en/trackingonline.php?noawb=' . $awb, [], $this->_getOptions());
+
+	public function checkShipment($awb)
+	{
+		$response = Requests::get($this->url . '/pcp.api/en/trackingonline.php?noawb=' . $awb, [], $this->_getOptions());
 		return $response->body;
 	}
-	
-	public function getShipmentDataTemplate() {
+
+	public function getShipmentDataTemplate()
+	{
 		return [
 			'allocation_code' => '',
 			'shipment_date_string' => '',
